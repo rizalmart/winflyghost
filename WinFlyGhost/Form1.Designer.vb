97588ff25c6340f57adc93c009cd8bf2110026de
rizalmartin <rizalmart98@gmail.com>
2025-11-11 13:03:58 +0800

    initial

---
+++ WinFlyGhost/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmmain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmmain))
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PackageName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BgEnabled = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.PackageName, Me.BgEnabled})
+        Me.DataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 27)
+        Me.DataGridView1.MultiSelect = False
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(678, 372)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(678, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'PackageName
+        '
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PackageName.DefaultCellStyle = DataGridViewCellStyle2
+        Me.PackageName.HeaderText = "Package Name"
+        Me.PackageName.Name = "PackageName"
+        Me.PackageName.Width = 500
+        '
+        'BgEnabled
+        '
+        Me.BgEnabled.HeaderText = "Enabled"
+        Me.BgEnabled.Name = "BgEnabled"
+        '
+        'frmmain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(678, 399)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmmain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "WinFlyGhost: Background App Manager"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PackageName As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents BgEnabled As System.Windows.Forms.DataGridViewCheckBoxColumn
+
+End Class
---
+++ WinFlyGhost/Form1.vb
@@ -0,0 +1,80 @@
+﻿Public Class frmmain
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.PopulateInstalledPackages()
+    End Sub
+
+    Sub PopulateInstalledPackages()
+
+        Dim mainKey As Microsoft.Win32.RegistryKey
+
+        mainKey = My.Computer.Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications")
+
+        If mainKey.SubKeyCount = 0 Then
+            MsgBox("No UWP Packages Installed", vbExclamation, "")
+            Exit Sub
+        End If
+
+        Dim subkeys() As String
+        Dim subkey As String
+        Dim newIndex As Long
+        Dim testkey As Microsoft.Win32.RegistryKey
+        Dim testval As Object
+
+        subkeys = mainKey.GetSubKeyNames()
+
+        For Each subkey In subkeys
+
+            testkey = My.Computer.Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications\" & subkey)
+            testval = testkey.GetValue("Disabled", 0)
+
+            newIndex = Me.DataGridView1.Rows.Add
+            Me.DataGridView1.Rows(newIndex).Cells(0).Value = subkey
+            Me.DataGridView1.Rows(newIndex).Cells(1).Value = 1 - testval
+
+        Next
+
+
+    End Sub
+
+
+
+    Sub ToggleSettings(RowIndex As Long)
+
+        Dim enval As Long
+        Dim testval2 As Object
+        Dim regkey As Microsoft.Win32.RegistryKey
+        Dim pkgname As String
+
+        pkgname = Me.DataGridView1.Rows(RowIndex).Cells(0).Value
+
+        regkey = My.Computer.Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications\" & pkgname, True)
+
+        If Me.DataGridView1.Rows(RowIndex).Cells(1).Value = 0 Then
+            enval = 1
+        Else
+            enval = 0
+        End If
+
+        'MsgBox(enval)
+
+        regkey.SetValue("Disabled", enval, Microsoft.Win32.RegistryValueKind.DWord)
+        regkey.SetValue("DisabledByUser", enval, Microsoft.Win32.RegistryValueKind.DWord)
+        regkey.SetValue("SleepDisabled", enval, Microsoft.Win32.RegistryValueKind.DWord)
+
+        regkey.Close()
+
+    End Sub
+
+
+    Private Sub DataGridView1_CurrentCellDirtyStateChanged(sender As Object, e As EventArgs) Handles DataGridView1.CurrentCellDirtyStateChanged
+        If DataGridView1.IsCurrentCellDirty Then
+            DataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit)
+            ToggleSettings(Me.DataGridView1.CurrentRow.Index)
+        End If
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        AboutBox1.Show()
+    End Sub
+End Class
